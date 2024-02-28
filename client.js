afc6e4c1d221c57db85c53bac353ad9d3958e36e
Zoe Lindman <zoe.lindman@gmail.com>
2024-02-27 19:40:39 -0600

    added submitForm function with increasing index value every entry

--- client.js
+++ client.js
@@ -1,1 +1,20 @@
 console.log('hello world');
+let currentIndex = 1;
+function submitForm(event){
+    event.preventDefault();
+    console.log('Form Submitted!');
+    let employeeTable = document.querySelector('#employeeData');
+    let employeeName = document.querySelector('#firstNameInput').value + ' ' + document.querySelector('#lastNameInput').value;
+    let employeeIdentification = document.querySelector('#employeeID').value;
+    let employeeJobTitle = document.querySelector('#jobTitle').value;
+    let employeeAnnualSalary = document.querySelector('#annualSalary').value;
+    employeeTable.innerHTML += `
+    <tr>
+        <td>${employeeName}</td>
+        <td>${employeeIdentification}</td>
+        <td>${employeeJobTitle}</td>
+        <td>${employeeAnnualSalary}</td>
+    </tr>
+    `
+    currentIndex +=1;
+}
