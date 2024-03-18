49abcd2362e6abcff0c59efad24ba2c49633327a
Zoe Lindman <zoe.lindman@gmail.com>
2024-03-18 14:17:01 -0500

    Applied red background CSS class to monthly totals that are over budget (20,000)

--- client.js
+++ client.js
@@ -1,13 +1,14 @@
 console.log('hello world');
 let currentIndex = 1;
+let totalSum = 0;
 function submitForm(event){
     event.preventDefault();
     console.log('Form Submitted!');
     let employeeTable = document.querySelector('#employeeData');
     let employeeName = document.querySelector('#firstNameInput').value + ' ' + document.querySelector('#lastNameInput').value;
-    let employeeIdentification = document.querySelector('#employeeID').value;
-    let employeeJobTitle = document.querySelector('#jobTitle').value;
-    let employeeAnnualSalary = document.querySelector('#annualSalary').value;
+    let employeeIdentification = document.querySelector('#idInput').value;
+    let employeeJobTitle = document.querySelector('#titleInput').value;
+    let employeeAnnualSalary = parseFloat(document.querySelector('#annualSalaryInput').value);
     employeeTable.innerHTML += `
     <tr>
         <td>${employeeName}</td>
@@ -17,10 +18,28 @@ function submitForm(event){
     </tr>
     `
     currentIndex +=1;
+    totalSum += employeeAnnualSalary;
+    document.getElementById('totalAnnualSalaries').innerHTML = `
+    Total Annual Salaries: ${totalSum}
+    `;
 }
 
 function clearForm(event){
     event.preventDefault();
     console.log('Form Cleared!');
     document.getElementById("employeeForm").reset();
+}
+
+function addMonthlySalaries(){
+    console.log('Calculating total...');
+    let monthlyTotal = totalSum / 12;
+    document.getElementById('total').innerHTML = `
+    ${monthlyTotal}
+    `;
+    if (monthlyTotal > 20000){
+        document.getElementById('totalMonthly').classList.add('overBudget');
+    }
+    else {
+        document.getElementById('totalMonthly').classList.remove('redBackground');
+    }
 }
